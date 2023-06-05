9ca24875b1a69d1bf246f76a6922eb1d748d9335
Daniel Sauer <danieljsauer92@gmail.com>
2023-06-05 10:03:39 -0600

    test: add instance method tests

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -18,12 +18,55 @@ describe Biker do
   end 
 
   describe "instance methods" do 
-    it " can learn new terrain" do 
+    it "can learn new terrain" do 
       biker = Biker.new("Kenny", 30)
       biker.learn_terrain!(:gravel)
       expect(biker.acceptable_terrain).to include(:gravel)
       biker.learn_terrain!(:hills)
       expect(biker.acceptable_terrain).to include(:gravel, :hills)
     end 
+
+    it "can log rides with ride object and time" do 
+      biker = Biker.new("Kenny", 30)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+      biker.learn_terrain!(:gravel)
+      biker.learn_terrain!(:hills)
+      biker.log_ride(ride1, 92.5)
+      biker.log_ride(ride1, 91.1)
+      biker.log_ride(ride2, 60.9)
+      biker.log_ride(ride2, 61.6)
+
+      expect(biker.rides).to eq({
+        ride1 => [92.5, 91.1],
+        ride2 => [60.9, 61.6]
+      })
+    end 
+
+    it "can only ride on known terrain" do 
+      biker = Biker.new("Kenny", 30)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      expect(biker.log_ride(ride1, 92.5)).to eq("#{biker.name} doesn't know this terrain yet!")
+    end 
+
+    it "can only ride within max distance" do 
+      biker = Biker.new("Kenny", 30)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      biker.learn_terrain!(:hills)
+      biker.ride1.add_leg
+      biker.ride1.add_leg
+      expect(biker.ride1.add_leg).to eq("#{biker.name} cannot ride that far!")
+
+    end 
+
+    it "can log PBs" do 
+      biker = Biker.new("Kenny", 30)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      biker.learn_terrain!(:hills)
+      biker.log_ride(ride1, 92.5)
+      biker.log_ride(ride1, 91.1)
+      expect(biker.personal_best(ride1)).to eq(91.1)
+    end 
+
   end 
 end 
