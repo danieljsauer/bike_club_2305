4de7d33ff7af7aedabc8e7284de4a37646cc1c3a
Daniel Sauer <danieljsauer92@gmail.com>
2023-06-05 11:01:33 -0600

    test: create initial tests for bike club

---
+++ spec/bike_club_spec.rb
@@ -0,0 +1,37 @@
+require './lib/ride'
+require './lib/biker'
+require 'pry'
+
+describe Biker do
+  describe "initialize" do 
+    it "exists" do 
+      club = BikeClub.new("Bridge 4")
+      expect(club).to be_instance_of(BikeClub)
+    end 
+
+    it "has a name" do 
+      club = BikeClub.new("Bridge 4")
+      expect(club.name).to eq("Bridge 4")
+    end 
+
+    it "can hold a list of bikers" do 
+      club = BikeClub.new("Bridge 4")
+      expect(club.bikers).to eq ([])
+    end 
+  end 
+
+  describe "instance methods" do 
+
+    it "can add bikers" do 
+      club = BikeClub.new("Bridge 4")
+      biker = Biker.new("Kenny", 30)
+      biker2 = Biker.new("Lilly", 15)
+      club.add_biker(biker)
+      club.add_biker(biker2)
+      expect(club.bikers.count).to eq(2)
+      expect(club.bikers).to include(biker, biker2)
+    end 
+
+  end 
+
+end 
