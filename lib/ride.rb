8de3049d47bdf653896913122a24959bee1a09fc
Daniel Sauer <danieljsauer92@gmail.com>
2023-06-05 09:11:19 -0600

    feat: initialize ride class

---
+++ lib/ride.rb
@@ -0,0 +1,13 @@
+class Ride 
+
+  attr_reader :name, :distance, :loop, :terrain
+
+  def initialize(ride_details)
+
+    @name = ride_details[:name]
+    @distance = ride_details[:distance]
+    @loop = ride_details[:loop]
+    @terrain = ride_details[:terrain]
+  end 
+
+end 
