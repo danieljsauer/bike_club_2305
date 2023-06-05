054eed90ea84df77d5a0cc256f19fc1a1cd2cf62
Daniel Sauer <danieljsauer92@gmail.com>
2023-06-05 11:28:38 -0600

    feat: most rides method

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -11,4 +11,23 @@ class BikeClub
     @bikers << biker 
   end 
 
+  def most_rides
+    return nil if @bikers.empty?
+  
+    most_rides_biker = ""
+    max_rides = 0
+  
+    @bikers.each do |biker|
+      ride_count = biker.rides.count
+      if ride_count > max_rides
+        max_rides = ride_count
+        most_rides_biker = biker
+      end
+    end
+  
+    most_rides_biker.name 
+  end
+  
+  
+
 end 
